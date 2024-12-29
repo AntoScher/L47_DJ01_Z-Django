78c1c2884d06d94098b572bf957dd68cbf644472
AntoScher <asc8er@gmail.com>
2024-12-29 20:41:30 +0300

    app-core

---
+++ zeroCoder/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+def contact(request):
+    return HttpResponse("<h1>Это контакты моего проекта на Django</h1>")

2bbb66a0eccbb15fc14175f34cdd8ddac2667217
AntoScher <asc8er@gmail.com>
2025-01-09 09:16:08 +0300

    final hw DJ03-DJ03. Создание приложения и работа с моделями

--- zeroCoder/data/models.py
+++ zeroCoder/data/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class News_post(models.Model):
 	title = models.CharField('Название новости', max_length=50)
 	short_description = models.CharField('Краткое описание новости', max_length=200)
-	#autor = models.CharField('Автор новости', max_length=200)
+	autor = models.CharField('Автор новости', max_length=200)
 	text = models.TextField('Новость')
 	pub_date = models.DateTimeField('Дата публикации')
 

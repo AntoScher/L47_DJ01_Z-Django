448eb3c76cccb44d1d96172f2693fd02bad34eec
AntoScher <asc8er@gmail.com>
2025-01-10 19:12:18 +0300

    префинал

---
+++ zeroCoder/data/forms.py
@@ -0,0 +1,17 @@
+from .models import News_post
+from django.forms import ModelForm,TextInput, DateTimeInput, Textarea
+
+class News_postForm(ModelForm):
+	class Meta:
+		model = News_post
+		fields = ['title', 'short_description','autor', 'text', 'pub_date']
+
+
+
+widgets = {
+        'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости', 'style': 'color: white;'}),
+        'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости', 'style': 'color: white;'}),
+        'autor': TextInput(attrs={'class': 'form-control', 'placeholder': 'Автор новости', 'style': 'color: white;'}),
+        'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости', 'style': 'color: white;'}),
+        'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации', 'style': 'color: white;'})
+    }
--- zeroCoder/data/views.py
+++ zeroCoder/data/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import News_post
-
+from .forms import News_postForm
 
 def data_view(request):
     news = News_post.objects.all()  # получить все новости из базы данных
     return render(request, 'data/data.html', {'caption1':"Белый орел",'news': news})
 def ad_data(request):
-    return render(request, 'data/addata.html', {'caption1':"Белый орел",'caption2':"птенцов орла"})
+    form=News_postForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return render(request, 'data/addata.html', {'caption1':"Белый орел",'caption2':"птенцов орла",'form': form})
 

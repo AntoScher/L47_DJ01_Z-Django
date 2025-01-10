c1d791db7637461315ce4ced352d8259a2717286
AntoScher <asc8er@gmail.com>
2025-01-10 20:07:39 +0300

    Update forms.py

--- zeroCoder/data/forms.py
+++ zeroCoder/data/forms.py
@@ -1,7 +1,19 @@
 from .models import News_post
-from django.forms import ModelForm,TextInput, DateTimeInput, Textarea
+from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
 class News_postForm(ModelForm):
+    class Meta:
+        model = News_post
+        fields = ['title', 'short_description', 'autor', 'text', 'pub_date']
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости', 'style': 'color: white;'}),
+            'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости', 'style': 'color: white;'}),
+            'autor': TextInput(attrs={'class': 'form-control', 'placeholder': 'Автор новости', 'style': 'color: white;'}),
+            'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости', 'style': 'color: white;'}),
+            'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации', 'style': 'color: white;'})
+        }
+
+"""class News_postForm(ModelForm):
 	class Meta:
 		model = News_post
 		fields = ['title', 'short_description','autor', 'text', 'pub_date']
@@ -9,9 +21,9 @@ class News_postForm(ModelForm):
 
 
 widgets = {
-        'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости', 'style': 'color: white;'}),
+        'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости', 'style': 'color: white !important;'}),
         'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости', 'style': 'color: white;'}),
         'autor': TextInput(attrs={'class': 'form-control', 'placeholder': 'Автор новости', 'style': 'color: white;'}),
         'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости', 'style': 'color: white;'}),
         'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации', 'style': 'color: white;'})
-    }
+    }"""

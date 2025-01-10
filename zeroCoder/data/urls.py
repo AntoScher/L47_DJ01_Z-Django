6cd447ad7dac0293eb3e039715b29db7b14caad5
AntoScher <asc8er@gmail.com>
2025-01-10 17:02:22 +0300

    news_page

--- zeroCoder/data/urls.py
+++ zeroCoder/data/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('data/', views.data_view, name='data'),
-    path('test/', views.test_view, name='test'),
+    path('test/', views.ad_data, name='test'),
 ]
 
 
--- zeroCoder/data/views.py
+++ zeroCoder/data/views.py
@@ -5,10 +5,6 @@ from .models import News_post
 def data_view(request):
     news = News_post.objects.all()  # получить все новости из базы данных
     return render(request, 'data/data.html', {'caption1':"Белый орел",'news': news})
-
-
-
-
-def test_view(request):
-    return render(request, 'data/test.html', {'caption1':"Белый орел",'caption2':"птенцов орла"})
+def ad_data(request):
+    return render(request, 'data/addata.html', {'caption1':"Белый орел",'caption2':"птенцов орла"})
 

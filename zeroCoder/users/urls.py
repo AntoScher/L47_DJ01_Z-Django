2938884431a2840cba822982e70eb3cded085ca6
AntoScher <asc8er@gmail.com>
2025-02-20 15:58:53 +0300

    no-save-profil

--- zeroCoder/users/urls.py
+++ zeroCoder/users/urls.py
@@ -34,5 +34,6 @@ urlpatterns = [
 
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
-    #path('accounts/profile/', views.ProfileUser.as_view(), name='profile'),
+
+
 ]
